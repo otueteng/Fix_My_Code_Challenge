e8005aa87be2c9dc7873944490fca4213269aacf
otueteng <otuofemeteng@gmail.com>
2023-08-16 05:53:44 +0000

    task done

---
+++ 0x01-challenge/react-blog/react-bootstrap-0.26.4/src/Accordion.js
@@ -0,0 +1,14 @@
+mport React from 'react';
+import PanelGroup from './PanelGroup';
+
+const Accordion = React.createClass({
+  render() {
+    return (
+      <PanelGroup {...this.props} accordion>
+        {this.props.children}
+      </PanelGroup>
+    );
+  }
+});
+
+export default Accordion;
